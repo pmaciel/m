7bd3d6936964d9d11d3da5377420f1477633d097
pmaciel <pmaciel@vub.ac.be>
2010-08-10 15:19:18 +0000

    t_manip: bugfix in second parameter of string of operands (not sure it works as expected, but suits well for the moment)

--- plugins/transform_collection/t_manip.cpp
+++ plugins/transform_collection/t_manip.cpp
@@ -4,7 +4,6 @@
 #include "mfunction.h"
 #include "t_manip.h"
 
-using namespace std;
 using namespace m;
 
 
@@ -20,9 +19,9 @@ Register< mtransform,t_manip > mt_manip(7,"-tvrm", "[str:...] variable removal, 
 void t_manip::transform(GetPot& o, mmesh& m)
 {
   // get key and value
-  const string k = o[o.get_cursor()];
-  const vector< pair< string,string > > vops = getoperations(o.get(o.inc_cursor(),""));
-  for (vector< pair< string,string > >::const_iterator i=vops.begin(); i<vops.end(); ++i) {
+  const std::string k = o[o.get_cursor()];
+  const std::vector< std::pair< std::string,std::string > > vops = getoperations(o.get(o.inc_cursor(),""));
+  for (std::vector< std::pair< std::string,std::string > >::const_iterator i=vops.begin(); i<vops.end(); ++i) {
          if (k=="-tvrm")  { vrm(m,getvindex(m,i->first)); }
     else if (k=="-tvmv")  { vmv(m,getvindex(m,i->first),getvindex(m,i->second)); }
     else if (k=="-tvren") { vren(m,getvindex(m,i->first),i->second); }
@@ -46,28 +45,28 @@ void t_manip::vmv(mmesh& m, const unsigned i, const unsigned j)
   vrm(m,i<j? i:i+1);
 }
 
-void t_manip::vren(mmesh& m, const unsigned i, const string& n)
+void t_manip::vren(mmesh& m, const unsigned i, const std::string& n)
 {
   m.vn[i] = n;
 }
 
-void t_manip::vadd(mmesh& m, const string& n, const string& f)
+void t_manip::vadd(mmesh& m, const std::string& n, const std::string& f)
 {
   // add a new variable (with zeros) in the end
   const unsigned Ndim  = m.d();
   const unsigned Nnode = m.n();
   m.vn.push_back(n);
-  m.vv.push_back(vector< double >(Nnode,0.));
+  m.vv.push_back(std::vector< double >(Nnode,0.));
   if (!f.length())
     return;
 
   // set mfunction (uses only the coordinates variables, for your own safety)
-  const vector< string > vnames(m.vn.begin(),m.vn.begin()+Ndim);
+  const std::vector< std::string > vnames(m.vn.begin(),m.vn.begin()+Ndim);
   mfunction mf(f,vnames);
 
   // evaluate at each node
-  vector< double >& v = m.vv.back();
-  vector< double > c(Ndim,0.);
+  std::vector< double >& v = m.vv.back();
+  std::vector< double > c(Ndim,0.);
   for (unsigned n=0; n<Nnode; ++n) {
     for (unsigned d=0; d<Ndim; ++d)
       c[d] = m.vv[d][n];
@@ -86,48 +85,54 @@ void t_manip::zmv(mmesh& m, const unsigned i, const unsigned j)
   zrm(m,i<j? i:i+1);
 }
 
-void t_manip::zren(mmesh& m, const unsigned i, const string& n)
+void t_manip::zren(mmesh& m, const unsigned i, const std::string& n)
 {
   m.vz[i].n = n;
 }
 
-unsigned t_manip::getvindex(const m::mmesh& m, const string& n)
+unsigned t_manip::getvindex(const mmesh& m, const std::string& n)
 {
   for (unsigned i=0; i<m.vn.size(); ++i)
     if (n==m.vn[i])
       return i;
-  cerr << "error: variable name not found: \"" << n << "\"" << endl;
+  std::cerr << "error: variable name not found: \"" << n << "\"" << std::endl;
   throw 42;
   return 0;
 }
 
-unsigned t_manip::getzindex(const m::mmesh& m, const string& n)
+unsigned t_manip::getzindex(const mmesh& m, const std::string& n)
 {
   for (unsigned i=0; i<m.vz.size(); ++i)
     if (n==m.vz[i].n)
       return i;
-  cerr << "error: zone name not found: \"" << n << "\"" << endl;
+  std::cerr << "error: zone name not found: \"" << n << "\"" << std::endl;
   throw 42;
   return 0;
 }
 
-vector< pair< string,string > > t_manip::getoperations(const string& s)
+std::vector< std::pair< std::string,std::string > >
+  t_manip::getoperations(const std::string& s)
 {
-  // split string find a '=' then a ':'
-  vector< pair< string,string > > r;
-  string::size_type p1 = 0,
-                    p2 = 0;
-  while (p2!=string::npos) {
-    pair< string,string > p("","");
+  using std::string;
 
-    p2 = min(s.find(":",p1),s.find("=",p1));
+  // split string find a '=' then a ':'
+  std::vector< std::pair< string,string > > r;
+  string::size_type p1 = 0,
+                         p2 = 0;
+  while (p2!=string::npos) {
+    std::pair< string,string > p("","");
+
+    p2 = std::min(s.find(":",p1),s.find("=",p1));
     p.first = s.substr(p1,(p2==string::npos? p2:p2-p1));
     if (s.find("=",p1)<s.find(":",p1)) {
       p1 = p2+1;
       p2 = s.find(":",p1);
-      string s2 = s.substr(p1,(p2==string::npos? p2:p2-p1));
-      istringstream ss(s2);
-      ss >> p.second;
+      p.second = s.substr(p1,(p2==string::npos? p2:p2-p1));
+      /* older version, maybe works better
+        string s2 = s.substr(p1,(p2==string::npos? p2:p2-p1));
+        istringstream ss(s2);
+        ss >> p.second;
+      */
     }
 
     p1 = p2+1;
