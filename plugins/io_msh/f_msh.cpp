447cbd8bff058cdd720b2f6b11213ea8254f622b
Pedro Maciel <pmbmaciel@gmail.com>
2014-03-24 00:51:12 +0100

    sip for the gmsh reader

--- plugins/io_msh/f_msh.cpp
+++ plugins/io_msh/f_msh.cpp
@@ -1,7 +1,10 @@
+#include <fstream>
+#include <sstream>
+
+#include <boost/lexical_cast.hpp>
 
 #include "mfactory.h"
 #include "f_msh.h"
-#include <sstream>
 
 using namespace std;
 using namespace m;
@@ -13,8 +16,8 @@ Register< mfinput,f_msh > mf_msh(".msh","Gmsh mesh input format");
 namespace aux {
 
 
-enum        msh_section_t     { NO_SECTION=0,  MESHFORMAT,   NODES,   ELEMENTS,   PERIODIC,   PHYSICALNAMES,   NODEDATA,   ELEMENTDATA,   INTERPOLATIONSCHEME,   ALL_SECTIONS };
-const char* msh_section_n[] = { "",           "MeshFormat", "Nodes", "Elements", "Periodic", "PhysicalNames", "NodeData", "ElementData", "InterpolationScheme", "" };
+enum        msh_section_t     { NO_SECTION=0,  MESHFORMAT,   NODES,   ELEMENTS,   PERIODIC,   PHYSICALNAMES,   NODEDATA,   ELEMENTDATA,   INTERPOLATIONSCHEME,   COMMENTS,   ALL_SECTIONS };
+const char* msh_section_n[] = { "",           "MeshFormat", "Nodes", "Elements", "Periodic", "PhysicalNames", "NodeData", "ElementData", "InterpolationScheme", "Comments", "" };
 
 
 const int element_t[] = {
@@ -74,16 +77,7 @@ const unsigned element_n[] = {
     0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,
     0,0,
-   64, 125 
-};
-
-
-template< int S=NO_SECTION >
-struct msh_section_reader {
-  msh_section_reader() : sbegin(msh_section_n[S]), send("End"+sbegin) {}
-  const std::string
-    sbegin,
-    send;
+   64, 125
 };
 
 
@@ -94,7 +88,7 @@ struct msh_element_reader {
 };
 
 
-}
+}  // namespace aux
 
 
 void f_msh::read(GetPot& o,mmesh& m)
@@ -107,9 +101,120 @@ void f_msh::read(GetPot& o,mmesh& m)
   }
 
 
-  // here
+  // read per section (magic!)
+  m.vn.clear();
+  m.vv.clear();
+  m.vz.clear();
+  while (f) {
+    string s;
+    f >> s;
+    const string end("$End"+(s.length()? s.substr(1):s));
+    s=="$MeshFormat"?          readMeshFormat          (end,f,m) :
+    s=="$Nodes"?               readNodes               (end,f,m) :
+    s=="$Elements"?            readElements            (end,f,m) :
+    s=="$Periodic"?            readPeriodic            (end,f,m) :
+    s=="$PhysicalNames"?       readPhysicalNames       (end,f,m) :
+    s=="$NodeData"?            readNodeData            (end,f,m) :
+    s=="$ElementData"?         readElementData         (end,f,m) :
+    s=="$InterpolationScheme"? readInterpolationScheme (end,f,m) :
+    s=="$Comments"?            readComments(end,f) :
+                               readError(f);
+  }
 
 
   f.close();
 }
 
+
+void f_msh::readMeshFormat(const string& end, ifstream& f, mmesh& m)
+{
+  double versionnumber(-1.);
+  int filetype(-1), datasize(-1);
+  string stop;
+  f? (f>>versionnumber)? (f>>filetype)? (f>>datasize)? (f>>stop)? : f:f:f:f:f;
+  if ( ((versionnumber!=2.2) && (cerr << "msh: warning: version-number!=2.2"  << endl)) ||
+       ((filetype!=0)        && (cerr << "msh: warning: file-type!=0"         << endl)) ||
+       ((datasize!=8)        && (cerr << "msh: warning: data-size!=8"         << endl)) ||
+       ((stop!=end)          && (cerr << "msh: warning: section not finished" << endl)) )
+    f.close();
+}
+
+
+void f_msh::readNodes(const string& end, ifstream& f, mmesh& m)
+{
+  unsigned numberofnodes(0), n(1);
+  f? (f>>numberofnodes) : f;
+
+  m.vn = vector< string >(3,"x");  m.vn[1]="y";  m.vn[2]="z";
+  m.vv.assign(3,vector< double >(numberofnodes,0.));
+
+  while (f && n>=1 && n<=numberofnodes) {
+    f >> n;
+    f >> m.vv[0][n-1] >> m.vv[1][n-1] >> m.vv[2][n-1];
+  }
+}
+
+
+void f_msh::readElements(const string& end, ifstream& f, mmesh& m)
+{
+  unsigned numberofelements(0), elmnumber(1), elmtype(0);
+  f? (f>>numberofelements) : f;
+
+  //FIXME does not support mixed element types
+  m.vz.push_back(m::mzone());
+  mzone& z = m.vz.back();
+  z.n = "Zone"+boost::lexical_cast< string >(m.vz.size());
+  for (; (f>>elmnumber>>elmtype) && (elmnumber>=1) && (elmnumber<=numberofelements); ) {
+
+    z.t = static_cast< mtype >(aux::element_t[elmtype]);
+    if (numberofelements && !(z.e2n[0].n.size())) {
+      melem e;
+      e.n.assign(aux::element_n[elmtype],0);
+      z.e2n.assign(numberofelements,e);
+    }
+
+    vector< unsigned > &n = z.e2n[elmnumber-1].n;
+    n.assign(aux::element_n[elmtype],0);
+    for (size_t i=0; i<n.size(); ++i)
+      f >> n[i];
+
+  }
+}
+
+
+void f_msh::readPeriodic(const string& end, ifstream& f, const mmesh& m)
+{
+}
+
+
+void f_msh::readPhysicalNames(const string& end, ifstream& f, mmesh& m)
+{
+}
+
+
+void f_msh::readNodeData(const string& end, ifstream& f, mmesh& m)
+{
+}
+
+
+void f_msh::readElementData(const string& end, ifstream& f, const mmesh& m)
+{
+}
+
+
+void f_msh::readInterpolationScheme(const string& end, ifstream& f, const mmesh& m)
+{
+}
+
+
+void f_msh::readComments(const string& end, ifstream& f)
+{
+  string s;
+  while ((f>>s) && (s!=end)) {};
+}
+
+
+void f_msh::readError(ifstream& f)
+{
+  f.close();
+}
--- plugins/io_msh/f_msh.h
+++ plugins/io_msh/f_msh.h
@@ -3,10 +3,26 @@
 
 #include "mkernel.h"
 
+namespace std { class fstream; }
+
 // module for .msh
 class f_msh : public m::mfinput {
+
  public:
   void read(GetPot& o, m::mmesh& m);
+
+ private:
+  void readMeshFormat          (const std::string& end, std::ifstream& f, m::mmesh& m);
+  void readNodes               (const std::string& end, std::ifstream& f, m::mmesh& m);
+  void readElements            (const std::string& end, std::ifstream& f, m::mmesh& m);
+  void readPeriodic            (const std::string& end, std::ifstream& f, const m::mmesh& m);
+  void readPhysicalNames       (const std::string& end, std::ifstream& f, m::mmesh& m);
+  void readNodeData            (const std::string& end, std::ifstream& f, m::mmesh& m);
+  void readElementData         (const std::string& end, std::ifstream& f, const m::mmesh& m);
+  void readInterpolationScheme (const std::string& end, std::ifstream& f, const m::mmesh& m);
+  void readComments            (const std::string& end, std::ifstream& f);
+  void readError               (std::ifstream& f);
+
 };
 
 #endif
