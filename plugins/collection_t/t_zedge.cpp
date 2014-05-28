54dbb917bcd3921ebb5294581200652b1ff9dca5
Pedro Maciel <pmbmaciel@gmail.com>
2014-05-28 16:23:39 +0200

    [FIX] finished zone edge detection, less lines and it works! time to merge.

--- plugins/collection_t/t_zedge.cpp
+++ plugins/collection_t/t_zedge.cpp
@@ -43,76 +43,58 @@ void t_zedge::transform(GetPot& o, mmesh& m)
   std::cout << "::zone edge..." << std::endl;
 
 
-  // find index of named zones
+  // append a edges zone for each input zone
   // NOTE: done by index, for modifying the zones vector invalidates references/iterators
   vector< string > znames(utils::split(o.get(o.inc_cursor(),""),':'));
   for (vector< string >::const_iterator n=znames.begin(); n!=znames.end(); ++n) {
-    try {
+    unsigned idx;
+    try                { idx = utils::getzindex(m,*n); }
+    catch (const int&) { continue; }
+    mzone& z = m.vz[idx];
 
-      // check if zones exist and comply with requirements (2D)
-      const unsigned i(utils::getzindex(m,*n));
-      if (m.d(i)!=2)
-        std::cerr << "warn: zone \"" << *n << "\": not 2D, skipped" << std::endl;
-        throw 42;
-
-      // build set of unique edges
-      // NOTE: avoid duplicates by attempting to remove them first
-      std::set< aux::eedge, aux::eedge_lt > edges;
-      for (vector< melem >::const_iterator eli=m.vz[i].e2n.begin(); eli!=m.vz[i].e2n.end(); ++eli) {
-
-        vector< aux::eedge > edges_in;
-        switch (m.vz[i].t) {
-
-          case (m::FETRIANGLE):
-            edges_in.push_back(std::make_pair(eli->n[0],eli->n[1]));
-            edges_in.push_back(std::make_pair(eli->n[1],eli->n[2]));
-            edges_in.push_back(std::make_pair(eli->n[2],eli->n[0]));
-            break;
-
-          case (m::FEQUADRILATERAL):
-            edges_in.push_back(std::make_pair(eli->n[0],eli->n[1]));
-            edges_in.push_back(std::make_pair(eli->n[1],eli->n[2]));
-            edges_in.push_back(std::make_pair(eli->n[2],eli->n[3]));
-            edges_in.push_back(std::make_pair(eli->n[3],eli->n[0]));
-            break;
-
-          case (m::FEPOLYGON):
-            std::cerr << "warn: zone \"" << *n << "\": polygonal zones not supported" << std::endl;
-            throw 42;
-            break;
-
-          default:
-            break;
-
-        }
-        for (vector< aux::eedge >::const_iterator ei=edges_in.begin(); ei!=edges_in.end(); ++ei)
-          if (!edges.erase(*ei))
-            edges.insert(*ei);
-
+    // build set of the zone unique edges
+    // (if an element edge exists already, remove it instead)
+    std::set< aux::eedge, aux::eedge_lt > edges;
+    for (vector< melem >::const_iterator eli=z.e2n.begin(); z.d()==2 && eli!=z.e2n.end(); ++eli) {
+      vector< aux::eedge > eedges;
+      switch (z.t) {
+        case (m::FETRIANGLE):
+          eedges.push_back(std::make_pair(eli->n[0],eli->n[1]));
+          eedges.push_back(std::make_pair(eli->n[1],eli->n[2]));
+          eedges.push_back(std::make_pair(eli->n[2],eli->n[0]));
+          break;
+        case (m::FEQUADRILATERAL):
+          eedges.push_back(std::make_pair(eli->n[0],eli->n[1]));
+          eedges.push_back(std::make_pair(eli->n[1],eli->n[2]));
+          eedges.push_back(std::make_pair(eli->n[2],eli->n[3]));
+          eedges.push_back(std::make_pair(eli->n[3],eli->n[0]));
+          break;
+        case (m::FEPOLYGON):
+        default:
+          break;
       }
-
-      // add surviving edges to a (new) destination zone connectivity table
-      if (!edges.empty()) {
-        m.vz.push_back(mzone(*n+"_edge",m::FELINESEG));
-        vector< melem > &e2n = m.vz.back().e2n;
-        melem e;
-        e.n.resize(2);
-        e2n.reserve(edges.size());
-        for (std::set< aux::eedge, aux::eedge_lt >::const_iterator ei=edges.begin(); ei!=edges.end(); ++ei) {
-          e.n[0] = ei->first;
-          e.n[1] = ei->second;
-          e2n.push_back(e);
-        }
-        std::cout << "info: zone \"" << *n << "\" edges: " << e2n.size() << std::endl;
-      }
-      else {
-        std::cerr << "warn: zone \"" << *n << "\": no edges found" << std::endl;
-      }
-
+      for (vector< aux::eedge >::const_iterator edi=eedges.begin(); edi!=eedges.end(); ++edi)
+        edges.erase(*edi)? true : edges.insert(*edi).second;
     }
-    catch (const int&) {
-      std::cerr << "warn: skipping zone \"" << *n << "\"" << std::endl;
+
+    // add surviving edges to a (new) destination zone connectivity table
+    if (edges.empty()) {
+      std::cerr << "info: zone \"" << *n << "\" edges: 0 (only non-polygonal 2D zones are supported)" << std::endl;
     }
+    else {
+      m.vz.push_back(mzone(*n+"_edge",m::FELINESEG));
+      vector< melem > &e2n = m.vz.back().e2n;
+      melem e;
+      e.n.resize(2);
+      e2n.reserve(edges.size());
+      for (std::set< aux::eedge, aux::eedge_lt >::const_iterator ei=edges.begin(); ei!=edges.end(); ++ei) {
+        e.n[0] = ei->first;
+        e.n[1] = ei->second;
+        e2n.push_back(e);
+      }
+      std::cout << "info: zone \"" << *n << "\" edges: " << edges.size() << std::endl;
+    }
+
   }
 
 
