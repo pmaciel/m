41695f58b7d60ef671df5307e04109d3913ec49d
Pedro Maciel <pmbmaciel@gmail.com>
2014-06-02 12:57:00 +0200

    [DEV] first version of zone intersection, node-based.

--- plugins/collection_t/t_zbool.cpp
+++ plugins/collection_t/t_zbool.cpp
@@ -34,13 +34,8 @@ void t_zbool::transform(GetPot& o, mmesh& m)
   }
 
   if (zidx.size()>1) {
-    const unsigned e1 = m.vz[ zidx[0] ].e2n.size();
-
     if      (k=="-tzunion") { zunion(m,zidx); }
     else if (k=="-tzinter") { zinter(m,zidx); }
-
-    const unsigned e2 = m.vz[ zidx[0] ].e2n.size();
-    cout << "info: zone \"" << m.vz[ zidx[0] ].n << "\" [e]: " << e1 << '>' << e2 << endl;
   }
   else {
     cerr << "warn: nothing to do.";
@@ -74,8 +69,9 @@ void t_zbool::zunion(mmesh& m, const std::vector< unsigned >& zidx)
   // do an hostile take-over
   for (unsigned i=1, k=Nelemi; i<zidx.size(); ++i) {
     for (unsigned j=0; j<m.vz[ zidx[i] ].e2n.size(); ++j) {
-      vector< unsigned >& en1 = z.e2n[k++].n;
-      vector< unsigned >& en2 = m.vz[ zidx[i] ].e2n[j].n;
+      vector< unsigned >
+       &en1 = z.e2n[k++].n,
+       &en2 = m.vz[ zidx[i] ].e2n[j].n;
       en1.swap(en2);
     }
   }
@@ -83,9 +79,44 @@ void t_zbool::zunion(mmesh& m, const std::vector< unsigned >& zidx)
   // delete useless zones
   for (unsigned i=zidx.size()-1; i>=1; --i)
     m.vz.erase( m.vz.begin()+zidx[i] );
+
+  cout << "info: zone \"" << z.n << "\" [e]: " << Nelemi << '>' << Nelemf << endl;
 }
 
 
 void t_zbool::zinter(mmesh& m, const std::vector< unsigned >& zidx)
 {
+  using namespace std;
+
+  // create list of usable nodes for the intersected zone
+  // (also, generate a name for the new zone with the intersected elements)
+  vector< bool > oknodes(m.n(),false);
+  string n(m.vz[ zidx[0] ].n);
+  for (vector< unsigned >::const_iterator i=zidx.begin()+1; i!=zidx.end(); ++i) {
+    n += "_inter_" + m.vz[*i].n;
+    for (vector< melem >::const_iterator j=m.vz[*i].e2n.begin(); j!=m.vz[*i].e2n.end(); ++j)
+      for (vector< unsigned >::const_iterator k=j->n.begin(); k!=j->n.end(); ++k)
+        oknodes[*k] = true;
+  }
+
+  // create new zone, intersecting the first with a union of the rest
+  mzone
+   &zi(m.vz[ zidx[0] ]),  // reference
+    zf(n,zi.t);           // (copy!)
+  for (vector< melem >::const_iterator j=zi.e2n.begin(); j!=zi.e2n.end(); ++j) {
+    bool okelem = true;
+    for (vector< unsigned >::const_iterator k=j->n.begin(); okelem && k!=j->n.end(); ++k)
+      okelem = oknodes[*k];
+    if (okelem)
+      zf.e2n.push_back(*j);
+  }
+
+  // append to mesh if elements are found, otherwise issue warning
+  if (zf.e2n.size()) {
+    m.vz.push_back(zf);
+    cout << "info: zone \"" << zf.n << "\" [e]: " << m.vz[ zidx[0] ].e2n.size() << '>' << m.vz.back().e2n.size() << endl;
+  }
+  else {
+    cerr << "warn: zone intersection generated no valid elements.";
+  }
 }
